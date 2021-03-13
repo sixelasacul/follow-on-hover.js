0971a0a8fb5c0222ce12e2369724a0a99d88e553
Alexis Lucas <alexis.lucas1@viacesi.fr>
2021-03-13 18:37:56 +0000

    Added babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,27 @@
+import { babel } from "@rollup/plugin-babel"
 import pkg from "./package.json"
 
 const src = "src/follow-on-hover.js"
 const baseOutput = {
-  name: "followOnHover",
-  file: pkg.browser,
-  format: "umd"
+	name: "followOnHover",
+	file: pkg.browser,
+	format: "umd"
 }
+const plugins = [babel({ babelHelpers: 'bundled' })]
 
 export default [
 	{
 		input: src,
-		output: baseOutput
+		output: baseOutput,
+		plugins
 	},
-  // For docs
+	// For docs
 	{
 		input: src,
 		output: {
 			...baseOutput,
-      file: `docs/${pkg.browser}`
-		}
+			file: `docs/${pkg.browser}`
+		},
+		plugins
 	}
 ]
